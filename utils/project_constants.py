6dc722bca375fdda87ba7abf6abaf43ff2b34c2b
pbhoje <p.bhoje@demos-foundation.org>
2026-03-02 00:37:38 +0530

    table creation & sales data write into CSV file through sql done

--- utils/project_constants.py
+++ utils/project_constants.py
@@ -5,9 +5,20 @@ current_path = os.getcwd()
 
 # Required configuration files
 CONFIG_FILE = os.path.join(current_path, "custom_config", "config.ini")
+OUTPUT_FOLDER = os.path.join(current_path, "sample_data")
 
 # File names
+FILE_NAME = f"sales_analysis_data{str(datetime.datetime.now())[:-7].strip().replace(' ', '_')}"
+#print(FILE_NAME)
 
+# Log folder names
+SQL_SOLUTIONS = "sql_solutions"
+PANDAS_SOLUTIOINS = "pandas_solutions"
 
-# Table names
+# Database & Table names
+INTERVIEW = "interview"
 
+CUSTOMERS = "customers"
+SALES = "sales"
+ITEMS = "items"
+ORDERS = "orders"
